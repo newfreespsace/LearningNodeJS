6589dfb5f14221e93aada77b4413e0a0b9d7c825
lee <li_yii@163.com>
2023-01-02 22:01:13 +0800

    035

--- 1.js
+++ 1.js
@@ -1,7 +1,36 @@
-let a = {
-  a : 3,
-  b : 4
-};
+const crypto = require("crypto");
+const fs = require("fs");
+
+const start = Date.now();
+const times = 1000000;
+
+setTimeout(() => {
+  console.log("0");
+}, 0);
 
 
-console.log(a.a);
+setTimeout(() => {
+  console.log("500");
+}, 500);
+
+fs.readFile("a.txt", () => {
+  console.log("a");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "1 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "2 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "3 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+  crypto.pbkdf2Sync("password", "salt", times, 1024, "sha512");
+  console.log(Date.now() - start, "4 Passord encrypted");
+});

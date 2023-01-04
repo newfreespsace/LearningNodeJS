a2c18a35f032acca90bce65d276b528057072bd3
lee <li_yii@163.com>
2023-01-04 19:04:02 +0800

    post

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-  .status(200)
-  .json({ message: 'hello from the server side!' });
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID}, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) return console.log(err);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
+});
+
+
+
+
 const port = 3000;
-
-
-
-
-
-
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });

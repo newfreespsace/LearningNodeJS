38555aaa8d89f4a7e21da05fbc82e64c53ea840f
lee <li_yii@163.com>
2023-01-04 22:01:13 +0800

    055

--- app.js
+++ app.js
@@ -53,6 +53,22 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+});
+
 
 
 

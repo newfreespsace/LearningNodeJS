9d463c706c2e1ee4dcd02eea935a8b2b5325d831
lee <li_yii@163.com>
2023-01-09 11:42:59 +0800

    0109

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 

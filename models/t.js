fcecb2ff0ae97fba3541730e13591469db7b1b9d
lee <zt4lxer@gmail.com>
2023-02-15 22:18:02 +0800

    0215

---
+++ models/t.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const User = require('./userModel');
+// const validator = require('validator');
+
+const tourSchema = new mongoose.Schema(
+  {
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
+  });
+
+请帮我解释下这段代码

ac85c36a37c301b31e9e8978ae485bf1c31de84f
lee <li_yii@163.com>
2023-03-01 09:13:03 +0800

    pug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const path = require('path');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,6 +12,9 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -29,6 +33,13 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTERS
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'abcd',
+    user: 'xxxx',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
